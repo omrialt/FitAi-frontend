51051a26b21e0367999e5532b24427a09c156457
omrialt <omrialt@gmail.com>
2025-12-17 21:58:36 +0200

    -remove unused hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -12,7 +12,6 @@
 
 // Authentication
 export { useAuth } from './useAuth';
-export { useRoleGuard, usePermission, useFeatureFlag } from './useRoleGuard';
 
 // API & Data Fetching
 export { useApi, useApiMutation } from './useApi';
@@ -20,30 +19,12 @@ export { useApi, useApiMutation } from './useApi';
 // Forms
 export { useFormHandler, useFormPersist } from './useFormHandler';
 
-// UI State
-export { useToggle, useMultiToggle } from './useToggle';
-export { useClickOutside, useClickInside, useFocusTrap } from './useClickOutside';
-
 // Performance
 export { useDebounce, useDebouncedCallback, useThrottle } from './useDebounce';
-
-// File Upload
-export { useUpload, useMultipleUpload } from './useUpload';
 
 // Export
 export { useExport } from './useExport';
 export { useNutritionExport } from './useNutritionExport';
 
-// Pagination
-export { usePagination, useInfinitePagination } from './usePagination';
-
-// Storage
-export { useLocalStorage, useSessionStorage, useStorageState } from './useLocalStorage';
-
-// Charts & Data Visualization
-export { useChart, useProgressChart, useWorkoutChart } from './useChart';
-
 // React 19 Features
 export { useMetadata, usePresetMetadata } from './useMetadata';
-export { useAuthContext, useOptionalAuth } from './useAuthContext';
-export { useResourcePreload } from './useResourcePreload';
